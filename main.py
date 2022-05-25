db5fc4b58f2a2672e6b6aceb4f076d437c36cb78
Georgios Dialynas-Vatsis <47365973+Sistav@users.noreply.github.com>
2022-05-25 05:40:38 -0400

    Added Player System and movement

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import sys, pygame
 pygame.init()
+window = pygame.display.set_mode((300, 300))
+clock = pygame.time.Clock()
 
-size = width, height = 1280, 720
+rect1 = pygame.Rect(0, 0, 20, 20)
+
+rect2 = pygame.Rect(0, 0, 20, 20)
+
+player1 = Player(pygame.K_w,pygame.K_a,pygame.K_s,pygame.K_d)
+player2 = Player(pygame.K_UP,pygame.K_LEFT,pygame.K_DOWN,pygame.K_RIGHT)
+
+run = True
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+        if event.type == pygame.KEYDOWN:
+            print(pygame.key.name(event.key))
+
+    keys = pygame.key.get_pressed()
+    
 
 black = 0, 0, 0
 
