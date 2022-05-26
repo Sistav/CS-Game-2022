e3a1efec9a52b45cb14cb6cbbc2e0cade9483d68
Georgios Dialynas-Vatsis <47365973+Sistav@users.noreply.github.com>
2022-05-25 23:05:54 -0400

    add some slope to bullets

--- bullet.py
+++ bullet.py
@@ -1,5 +1,8 @@
 class bullet:
-    def __init__(self):
-        self.x
-        self.y
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        # slopex
+        # slopey
         
+        # Decide on whether to use angle or slope to move bullet
