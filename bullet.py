4f97aa59b5621e97dee417ee1716bb606e51e30b
Georgios Dialynas-Vatsis <47365973+Sistav@users.noreply.github.com>
2022-05-26 13:27:42 -0400

    Added Proper bullet shots

--- bullet.py
+++ bullet.py
@@ -1,8 +1,31 @@
-class bullet:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-        # slopex
-        # slopey
+import math
+import pygame
+class Bullet:
+    bullets = []
+    def __init__(self,spawn_x,spawn_y,angle, color,spawn_tick):
+        Bullet.bullets.append(self)
+        self.x = spawn_x
+        self.y = spawn_y
         
-        # Decide on whether to use angle or slope to move bullet
+        self.angle = angle
+        self.color = color
+        
+        self.speed = 5
+        self.radius = 10
+        
+        self.spawn_tick = spawn_tick
+        
+        self.lifetime =  20 * 60
+
+    def move(self,window,current_cycle):
+        rad_angle = self.angle * math.pi / 180
+        self.x += self.speed * math.cos(rad_angle)
+        self.y += self.speed * -math.sin(rad_angle)
+
+        self.x = self.x % window.get_width()
+        self.y = self.y % window.get_height()
+
+        
+
+    def draw(self,window):
+        pygame.draw.circle(window,self.color,(self.x, self.y),self.radius)
