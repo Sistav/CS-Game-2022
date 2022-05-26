f45ba2e17546c944b00d70ed871058c433b3cd21
Georgios Dialynas-Vatsis <47365973+Sistav@users.noreply.github.com>
2022-05-25 22:58:04 -0400

    Added full OO on players

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ rect1 = pygame.Rect(0, 0, 20, 20)
 rect2 = pygame.Rect(0, 0, 20, 20)
 
 # Creates the player
-player1 = Player(pygame.K_w,pygame.K_a,pygame.K_s,pygame.K_d)
-player2 = Player(pygame.K_UP,pygame.K_LEFT,pygame.K_DOWN,pygame.K_RIGHT)
+player1 = Player([pygame.K_w,pygame.K_a,pygame.K_s,pygame.K_d],pygame.K_SPACE,[pygame.K_e,pygame.K_q],rect1,(255, 0, 0))
+player2 = Player([pygame.K_UP,pygame.K_LEFT,pygame.K_DOWN,pygame.K_RIGHT],pygame.K_SPACE,[pygame.K_e,pygame.K_q],rect2,(0, 0, 255))
+
+# player2 = Player()
 
 # Gameloop
 run = True
@@ -27,25 +29,17 @@ while run:
     keys = pygame.key.get_pressed()
     
     # Check player movement (need to make this a for loop)
-    player1.check_movement(keys)
-    player2.check_movement(keys)
+    for i in range(len(Player.players)):
+        Player.players[i].check_movement(keys,window)
 
-    rect1.x = player1.x
-    rect1.y = player1.y
-        
-    rect1.centerx = rect1.centerx % window.get_width()
-    rect1.centery = rect1.centery % window.get_height()
-
-    rect2.x = player2.x
-    rect2.y = player2.y
-        
-    rect2.centerx = rect2.centerx % window.get_width()
-    rect2.centery = rect2.centery % window.get_height()
 
     window.fill(0)
+    for i in range(len(Player.players)):
+        Player.players[i].draw(window)
+    # rect(window, (0, 0, 255), rect1)
+    # pygame.draw.rect(window, (255, 0, 0), rect2)
+    # pygame.draw.line(window, (100, 100, 255), (rect1.centerx, rect1.centery), (rect2.centerx, rect2.centery), 8) 
 
-    pygame.draw.rect(window, (0, 0, 255), rect1)
-    pygame.draw.rect(window, (255, 0, 0), rect2)
 
     pygame.display.flip()
 
--- player.py
+++ player.py
@@ -1,18 +1,30 @@
+import pygame
 class Player:
     players = []
-    def __init__(self,up,left,down,right):
+    def __init__(self,movement,shoot,aim,sprite,color):
         Player.players.append(self)
 
-        self.up = up
-        self.left = left
-        self.down = down
-        self.right = right
+        self.up = movement[0]
+        self.left = movement[1]
+        self.down = movement[2]
+        self.right = movement[3]
+
+
+        self.aim_right = aim[0]
+        self.aim_left = aim[1]
+
+        self.shoot = shoot
+    
 
         self.velocity = 5
         self.x = 10 
         self.y = 10
+        
+        self.sprite = sprite
+
+        self.color = color
     
-    def check_movement(self,keys):
+    def check_movement(self,keys,window):
             # if keys[self.left]:   
             #     self.x -= self.velocity
             # if keys[self.right]:
@@ -22,5 +34,14 @@ class Player:
             # if keys[self.down]:
             #     self.y += self.velocity
 
-            self.x += self.velocity * (keys[self.right] - keys[self.left])
-            self.y += self.velocity * (keys[self.down] - keys[self.up])
+            self.sprite.x += self.velocity * (keys[self.right] - keys[self.left])
+            self.sprite.y += self.velocity * (keys[self.down] - keys[self.up])
+
+            # self.sprite.x = self.x
+            # y = self.y
+                
+            self.sprite.centerx = self.sprite.centerx % window.get_width()
+            self.sprite.centery = self.sprite.centery % window.get_height()
+
+    def draw(self,window):
+        pygame.draw.rect(window,self.coloself.spriter)
